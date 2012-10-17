963a3a806111e40e5e609afba2eaab79e07eec36
orthodaos <orthodaos@gmail.com>
2012-10-18 01:04:28 +0300

    add fetch functions

--- src/urlrover/core.clj
+++ src/urlrover/core.clj
@@ -4,3 +4,31 @@
   "I don't do a whole lot."
   [& args]
   (println "Hello, World!"))
+
+(defn fetch-url[address]
+  (with-open [stream (.openStream (java.net.URL. address))]
+    (let  [buf (java.io.BufferedReader. 
+                (java.io.InputStreamReader. stream))]
+      (apply str (line-seq buf)))))
+
+(defn fetch-data [url]
+  (let  [con    (-> url java.net.URL. .openConnection)
+         fields (reduce (fn [h v] 
+                          (assoc h (.getKey v) (into [] (.getValue v))))
+                        {} (.getHeaderFields con))
+         size   (first (fields "Content-Length"))
+         in     (java.io.BufferedInputStream. (.getInputStream con))
+         out    (java.io.BufferedOutputStream. 
+                 (java.io.FileOutputStream. "out.file"))
+         buffer (make-array Byte/TYPE 1024)]
+    (loop [g (.read in buffer)
+           r 0]
+      (if-not (= g -1)
+        (do
+          (println r "/" size)
+          (.write out buffer 0 g)
+          (recur (.read in buffer) (+ r g)))))
+    (.close in)
+    (.close out)
+    (.disconnect con)))
+
